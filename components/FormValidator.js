26264b28ba707fea791ec3711ebd8ca77918226a
Corey Smith <coreysmithd4@gmail.com>
2024-09-14 17:16:21 -0700

    Project 7

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -36,11 +36,11 @@ class FormValidator {
   }
   
 
-  _toggleButtonState(inputEls, submitButton, config) {
+  _toggleButtonState(inputEls) {
       if (this._hasInvalidInput(inputEls)) {
-          this.disableButton(submitButton, config);
+          this.disableButton();
         } else {
-          this._enableButton(submitButton, config);
+          this._enableButton();
         }
   }
 
@@ -68,7 +68,7 @@ class FormValidator {
   enableValidation() {
       this._form.addEventListener("submit", (e) => {
           e.preventDefault();
-          this.disableButton(this._submitButton,this._config.inactiveButtonClass);
+          this.disableButton();
       });
 
       this._setEventListeners();
